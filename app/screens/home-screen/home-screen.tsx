a2189ed9bb82294d39ae8687214c6d92b97ab345
Pradeep Dan Charan <pradeep.charan@moonshinetechnology.com>
2024-08-04 12:46:59 +0530

    FEAT: Setup Code Quality Tools

---
+++ app/screens/home-screen/home-screen.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import { styles } from './home-screen.styles'
+import { Text, View } from 'react-native'
+
+export const HomeScreen = () => {
+  return (
+    <View>
+      <Text>Home Screen</Text>
+    </View>
+  )
+}
